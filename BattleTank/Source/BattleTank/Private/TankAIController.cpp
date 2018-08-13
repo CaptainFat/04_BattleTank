bdd9f07655f6ecae869e9c0feb0cf5d97466438a
DESKTOP-HVTGTJN\Benjamin <bdavidson6@liberty.edu>
2018-08-13 19:25:40 -0400

    BT23: Getting PlayerController from AIController

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Runtime/Engine/Classes/Engine/World.h"
 #include "TankAIController.h"
 
 
@@ -8,6 +9,8 @@ void ATankAIController::BeginPlay()
 	Super::BeginPlay();
 
 	auto AITank = GetAIControlledTank();
+	auto PlayerTank = GetPlayerTank();
+
 	if (!AITank)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("The Tank is not possessed"));
@@ -15,11 +18,23 @@ void ATankAIController::BeginPlay()
 	else
 	{
 		UE_LOG(LogTemp, Warning, TEXT("The Tank is possessed by: %s"), *(AITank->GetName()));
+			if (!PlayerTank)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("AI Cannot find PlayerController"));
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("AI found PlayerController: %s"), *(PlayerTank->GetName()))
+			}
 	}
-
 }
 
 ATank* ATankAIController::GetAIControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
 }
