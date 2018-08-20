bbd7d0c5850622c417f4969e5e10f0ae43b578ec
DESKTOP-HVTGTJN\Benjamin <bdavidson6@liberty.edu>
2018-08-20 16:56:52 -0400

    BT30: Created TankAimingComponent

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -11,19 +12,18 @@ class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-public:
-
-	
-
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	UTankAimingComponent * TankAimingComponent = nullptr;
+
 
 public:	
 	void AimAt(FVector HitLocation);
 
 
 private:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
